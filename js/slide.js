b2b72026cff4331c394c83f9488c3565bc741e06
AntoninaNadeina <obolon-prem@yandex.ru>
2015-04-22 13:48:29 +0300

    first commit

---
+++ js/slide.js
@@ -0,0 +1,36 @@
+(function($){
+    var viewText = $("div.all-text"),
+    divs = viewText.find('div.texts'),
+    divW = 313,
+    divsLen = divs.length,
+    totalDivsW = divW * divsLen,
+    current = 1;
+    $("div.slider-text").css('overflow','hidden');
+        console.log('ddd');
+
+    $("#show").find('button')
+        .on('click',function(){
+           var direction = $(this).attr('id');
+           var position = divW;
+            (direction === 'next') ? ++current : --current;
+            if(current === 0){
+                current = divsLen;
+                direction = 'next';
+                position = totalDivsW - divW;
+            } else if (current-1 === divsLen){
+                current = 1;
+                position = 0;
+            }
+            doIt(viewText, position, direction);
+        });
+
+    function doIt(container, position, direction){
+        var sign;
+        if(direction && position !==0){
+            sign = (direction === 'next')? '-=':'+=';
+        }
+        container.animate({
+            'margin-left': sign ? (sign+position) : position
+        });
+    }
+})(jQuery);
